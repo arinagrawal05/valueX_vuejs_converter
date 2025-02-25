a17a8daec83521e06729be690b508dcdeee1b920
Zuhair Ahmad <zuhair2945@gmail.com>
2025-02-25 10:20:40 +0530

    added script and templates

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import MainPage from '../components/MainPage.vue'
-
+// managing route paths
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
